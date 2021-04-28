c9d9e535905b47d1f16c57e0ffa40766a3fc330a
Ashwini Surve <ashwinisurve@Ashwinis-MacBook-Air.local>
2021-04-28 07:54:40 +0530

    add window focus event

--- background.js
+++ background.js
@@ -1,1 +1,18 @@
-window.addEventListener('offline', () => window.open("popup.html"));
+var offline = false;
+function listen() {
+    window.top.addEventListener('offline', () => {       
+        if(!offline) {
+            window.top.open("popup.html");
+            this.offline = true;  
+            setTimeout(()=> {
+                this.offline = false; 
+            },3000)
+        }
+    });
+}
+
+chrome.windows.onFocusChanged.addListener(function(window) {
+    if (!(window == chrome.windows.WINDOW_ID_NONE)) {       
+        listen();
+    }
+});
